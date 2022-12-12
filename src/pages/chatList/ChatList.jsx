95768d10669a9bd413b6880669ab1f259b616047
jhyun-k <jhkim1238@naver.com>
2022-12-12 13:58:02 +0900

    chatlist markup

--- src/pages/chatList/ChatList.jsx
+++ src/pages/chatList/ChatList.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
 export default function ChatList() {
-    return <div>ChatList</div>;
+  return (
+    <>
+      <main>
+        <ul>
+          <li>
+            <div>profile-img</div>
+            <div>
+              <strong>회원이름</strong>
+              <p>채팅내용</p>
+            </div>
+            <strong>채팅날짜</strong>
+          </li>
+        </ul>
+      </main>
+    </>
+  );
 }
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 import Login from '../pages/login/Login';
 import SignUp from '../pages/signup/Signup';
 import Splash from '../pages/splash/Splash';
+import ChatList from './../pages/chatList/ChatList';
+import ChatRoom from './../pages/chatRoom/ChatRoom';
 
 export default function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Splash />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/chatlist" element={<ChatList />} />
+        <Route path="/chatroom" element={<ChatRoom />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
