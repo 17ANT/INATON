8ca5b4c9ff7014ebb3a3871cf1699d6675ed4bb2
sewon kim <sewon0325@naver.com>
2022-12-12 13:24:17 +0900

    에셋 추가

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 
 export default function Login() {
     return <>
-    Login
+    <h2>로그인</h2>
+    <form action="">
+          <h2>Send Message</h2>
+          <div class="inputBox">
+            <input type="text" name="" required />
+            <span>FullName</span>
+          </div>
+          <div class="inputBox">
+            <input type="text" name="" required />
+            <span>Email</span>
+          </div>
+          <div class="inputBox">
+            <input type="submit" value="Send" />
+          </div>
+        </form>
+    <a href="#">이메일로 회원가입</a>
     </>;
 }
