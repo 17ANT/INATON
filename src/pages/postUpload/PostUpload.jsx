16aa9f7f2f3223a2f7388fca20b0a031e0fb2823
baek gyeonghyeon <baekg.dev@gmail.com>
2022-12-12 13:21:18 +0900

    Fix: pull 오류 테스트

--- src/pages/postUpload/PostUpload.jsx
+++ src/pages/postUpload/PostUpload.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.div`
+    height: 48px;
+    background-color: #ff9191;
+`;
+const PostUploadMain = styled.main`
+    display: flex;
+    height: 100%;
+    background-color: royalblue;
+`;
 
 export default function PostUpload() {
-    return <div>PostUpload</div>;
-    // test upload
+    return (
+        <>
+            <Header />
+            <PostUploadMain></PostUploadMain>
+        </>
+    );
 }
