35563243bf54791f2eef28c0f45d66b394809124
baek gyeonghyeon <baekg.dev@gmail.com>
2022-12-12 12:50:34 +0900

    Feat: postUpload 라우터 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Login from '../pages/login/Login';
 import SignUp from '../pages/signup/Signup';
 import Splash from '../pages/splash/Splash';
+import PostUpload from '../pages/postUpload/PostUpload';
 
 export default function Router() {
     return (
@@ -11,6 +12,7 @@ export default function Router() {
                 <Route path="/" element={<Splash />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
+                <Route path="/postupload" element={<PostUpload />} />
             </Routes>
         </BrowserRouter>
     );
