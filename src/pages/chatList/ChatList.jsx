31d59aa2f03a048aee26b5d44e00339d75cf7d49
jhyun-k <jhkim1238@naver.com>
2022-12-12 14:44:39 +0900

    Feat/chatlist

--- src/pages/chatList/ChatList.jsx
+++ src/pages/chatList/ChatList.jsx
@@ -1,20 +1,98 @@
 import React from 'react';
+import profile from '../../assets/Ellipse 6.png';
+import styled from 'styled-components';
+
+const ChatMain = styled.main`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  min-width: 390px;
+  width: 100%;
+  height: 100%;
+  padding: 24px 16px;
+`;
+
+const List = styled.ul`
+  width: 100%;
+  max-width: 390px;
+  li {
+    display: flex;
+    position: relative;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+  }
+  li .user-profile {
+    width: 42px;
+    height: 42px;
+    border: 0.5px solid #dbdbdb;
+    border-radius: 50%;
+    margin-right: 12px;
+  }
+  .user-profile img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 50%;
+  }
+  .chat-info strong {
+    display: block;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 18px;
+    margin: 2px 0 4px 0;
+  }
+  .chat-info p {
+    max-width: 238px;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 15px;
+    color: #767676;
+    display: block;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .chat-date {
+    position: absolute;
+    right: 0;
+    bottom: 3px;
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 13px;
+    color: #dbdbdb;
+  }
+`;
 
 export default function ChatList() {
   return (
     <>
-      <main>
-        <ul>
+      <ChatMain>
+        <List>
           <li>
-            <div>profile-img</div>
-            <div>
+            <div className="user-profile">
+              <img src={profile} alt="" />
+            </div>
+            <div className="chat-info">
+              <strong>회원이름</strong>
+              <p>
+                채팅내용채팅내용채팅내용채팅내용채팅내용채팅내용채팅내용채팅내용채팅내용
+              </p>
+            </div>
+            <strong className="chat-date">2022-12-12</strong>
+          </li>
+          <li>
+            <div className="user-profile">
+              <img src={profile} alt="" />
+            </div>
+            <div className="chat-info">
               <strong>회원이름</strong>
               <p>채팅내용</p>
             </div>
-            <strong>채팅날짜</strong>
+            <strong className="chat-date">채팅날짜</strong>
           </li>
-        </ul>
-      </main>
+        </List>
+      </ChatMain>
     </>
   );
 }
