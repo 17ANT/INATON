db0c88aebc1ad9ad0dd42531f5de2413e3155cba
jangsumin <jsm0726@naver.com>
2022-12-13 17:38:02 +0900

    Feat: 내 프로필 수정에 들어가는 label, input 요소 묶음 컴포넌트

---
+++ src/components/Jang/UserInfoInput.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const UserInputDiv = styled.div`
+    width: 322px;
+`;
+
+const Label = styled.label`
+    display: block;
+    font-size: 12px;
+    font-weight: 500;
+    color: var(--sub-font);
+`;
+
+const Input = styled.input`
+    margin-top: 10px;
+    padding-bottom: 8px;
+    width: 322px;
+    font-size: 14px;
+    font-weight: 400;
+    border: none;
+    border-bottom: 2px solid var(--border-color);
+
+    &:focus,
+    &:active {
+        outline: none;
+        border: none;
+        border-bottom: 2px solid var(--main-color);
+    }
+    &::placeholder {
+        font-size: 14px;
+        color: var(—border-color);
+    }
+`;
+
+export default function UserInfoInput({ labelText, placeholder }) {
+    return (
+        <UserInputDiv>
+            <Label>{labelText}</Label>
+            <Input placeholder={placeholder}></Input>
+        </UserInputDiv>
+    );
+}
