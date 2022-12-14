0f1aa2d068229f3832df93855f673d0b08657326
jhyun-k <jhkim1238@naver.com>
2022-12-14 17:51:54 +0900

    Style: style 수정

--- src/components/inlineProfileInfo/InlineProfileInfo.jsx
+++ src/components/inlineProfileInfo/InlineProfileInfo.jsx
@@ -5,19 +5,25 @@ import ProfileImg from '../profileImg/ProfileImg';
 const ProfileInfo = styled.div``;
 
 export default function InlineProfileInfo({ img, name, desc, state }) {
-    let size;
-    switch (state.toUpperCase()) {
-        case 'CHAT':
-            size = '42px';
-    }
-    return (
-        <ProfileInfo>
-            {img && <ProfileImg alt={`${name}님의 프로필 이미지`} size={size}></ProfileImg>}
-            <ProfileImg src={img} alt={`${name}님의 프로필 이미지`} size={size}></ProfileImg>
-        </ProfileInfo>
-    );
+  let size;
+  switch (state.toUpperCase()) {
+    case 'CHAT':
+      size = '42px';
+  }
+  return (
+    <ProfileInfo>
+      {img && (
+        <ProfileImg alt={`${name}님의 프로필 이미지`} size={size}></ProfileImg>
+      )}
+      <ProfileImg
+        src={img}
+        alt={`${name}님의 프로필 이미지`}
+        size={size}
+      ></ProfileImg>
+    </ProfileInfo>
+  );
 }
 
 ProfileImg.defaultProps = {
-    desc: null,
+  desc: null,
 };
