0c2a6025f180e2778a87461a3acf07a35733e8cc
jangsumin <jsm0726@naver.com>
2022-12-14 15:39:51 +0900

    Refactor: 하위 컴포넌트를 합성하여 내 프로필 페이지 재구성

--- src/pages/myProfile/MyProfile.jsx
+++ src/pages/myProfile/MyProfile.jsx
@@ -1,147 +1,71 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import BasicHeader from '../../components/header/BasicHeader';
+import FollowCount from '../../components/followCount/FollowCount';
+import ProfileImg from '../../components/profileImg/ProfileImg';
+import UserInfoText from '../../components/userInfoText/UserInfoText';
+import CustomButton from '../../components/customButton/CustomButton';
 
-const MyProfileDiv = styled.div`
+const MyProfileWrap = styled.div`
     margin: 0 auto;
+    padding-top: 50px;
     width: 100%;
+    min-width: 358px;
+    border-bottom: 6px solid var(--border-color);
 `;
 
-const TopBasicNav = styled.nav`
+const ProfileHeader = styled.div`
     display: flex;
-    justify-content: space-between;
-    min-width: 390px;
-    border-bottom: 0.5px solid var(--border-color);
-`;
-
-const ArrowLeftImg = styled.img`
-    margin: 13px 0 13px 16px;
-    width: 22px;
-    height: 22px;
-`;
-
-const MoreVerticalImg = styled.img`
-    margin: 12px 12px 12px 0;
-    width: 24px;
-    height: 24px;
-`;
-
-const ProfileSection = styled.section`
     margin: 0 auto;
-    padding: 30px 16px 26px;
-    width: fit-content;
-`;
-
-const FollowDiv = styled.div`
-    display: flex;
-    width: 275px;
-    justify-content: center;
+    padding-top: 30px;
+    justify-content: space-between;
     align-items: center;
+    gap: 41px;
+    width: 275px;
 `;
 
-const FollowersDiv = styled.div`
+const ProfileMain = styled.div`
+    margin: 0 auto;
+    padding-top: 16px;
+    width: 358px;
+`;
+
+const ProfileButton = styled.div`
+    margin: 0 auto;
+    padding: 24px 0;
+    width: 144px;
     text-align: center;
-`;
-
-const FollowersNum = styled.p`
-    margin: 6px;
-    font-size: 18px;
-    font-weight: 700;
-`;
-
-const FollowersStr = styled.p`
-    font-size: 8px;
-    font-weight: 400;
-    color: var(--sub-font);
-`;
-
-const ProfileImg = styled.img`
-    margin: 0 41px;
-    width: 110px;
-    height: 110px;
-`;
-
-const FollowingsDiv = styled.div`
-    text-align: center;
-`;
-
-const FollowingsNum = styled.p`
-    margin: 6px;
-    font-size: 18px;
-    font-weight: 700;
-    color: var(--sub-font);
-`;
-
-const FollowingsStr = styled.p`
-    font-size: 8px;
-    font-weight: 400;
-    color: var(--sub-font);
-`;
-
-const UserNameHeader = styled.h1`
-    margin-top: 16px;
-    text-align: center;
-    font-size: 17px;
-    font-weight: 700;
-`;
-
-const UserIdSpan = styled.span`
-    margin-top: 6px;
-    display: block;
-    text-align: center;
-    font-size: 12px;
-    font-weight: 400;
-    color: var(--sub-font);
-`;
-
-const UserDescSpan = styled.span`
-    margin-top: 16px;
-    display: block;
-    text-align: center;
-    font-size: 14px;
-    font-weight: 400;
-    color: var(--sub-font);
-`;
-
-const ProfileModificationBtn = styled.div`
-    margin: 24px auto 0 auto;
-    padding: 8px 26px;
-    width: fit-content;
-    font-size: 14px;
-    font-weight: 500;
-    color: var(--sub-font);
-    border: 1px solid var(--border-color);
-    border-radius: 30px;
 `;
 
 export default function MyProfile() {
     return (
-        <MyProfileDiv>
-            <TopBasicNav>
-                <ArrowLeftImg src='/assets/icon/icon-arrow-left.png' alt='arrow-left-img' />
-                <MoreVerticalImg src='/assets/icon/icon-more-vertical.png' alt='more-vertical-img' />
-            </TopBasicNav>
+        <>
+            <BasicHeader></BasicHeader>
 
-            <ProfileSection>
-                <FollowDiv>
-                    <FollowersDiv>
-                        <FollowersNum>2950</FollowersNum>
-                        <FollowersStr>followers</FollowersStr>
-                    </FollowersDiv>
-                    <ProfileImg src='/assets/Ellipse-1.png'></ProfileImg>
-                    <FollowingsDiv>
-                        <FollowingsNum>128</FollowingsNum>
-                        <FollowingsStr>followings</FollowingsStr>
-                    </FollowingsDiv>
-                </FollowDiv>
+            <MyProfileWrap>
+                <ProfileHeader>
+                    <FollowCount count={2950} kind='followers'></FollowCount>
+                    <ProfileImg size='110px' src='assets/Ellipse-1.png' alt='profile image'></ProfileImg>
+                    <FollowCount count={128} kind='followings'></FollowCount>
+                </ProfileHeader>
 
-                <UserNameHeader>애월읍 위니브 감귤농장</UserNameHeader>
-                <UserIdSpan>@weniv_Mandarin</UserIdSpan>
-                <UserDescSpan>애월읍 감귤 전국 배송, 귤따기 체험, 감귤 농장</UserDescSpan>
-                <Link to='/myprofile/modification' style={{ textDecoration: 'none' }}>
-                    <ProfileModificationBtn>프로필 수정</ProfileModificationBtn>
-                </Link>
-            </ProfileSection>
-        </MyProfileDiv>
+                <ProfileMain>
+                    <UserInfoText
+                        userName='애월읍 위니브 감귤농장'
+                        userId='@ weniv_Mandarin'
+                        userDesc='애월읍 감귤 전국 배송, 귤따기 체험, 감귤 농장'
+                    ></UserInfoText>
+                </ProfileMain>
+
+                <ProfileButton>
+                    <Link to='/myprofile/modification'>
+                        <CustomButton size='M' state='activ'>
+                            프로필 수정
+                        </CustomButton>
+                    </Link>
+                </ProfileButton>
+            </MyProfileWrap>
+        </>
     );
 }
