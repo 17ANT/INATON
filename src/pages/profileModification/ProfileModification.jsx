4d82ce2e5d2173d534a520772e2b5e33b48f0b98
jangsumin <jsm0726@naver.com>
2022-12-14 16:10:25 +0900

    Feat: 하위 컴포넌트 합성으로 내 프로필 수정 페이지 재구성

--- src/pages/profileModification/ProfileModification.jsx
+++ src/pages/profileModification/ProfileModification.jsx
@@ -1,135 +1,69 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import UploadHeader from '../../components/header/UploadHeader';
+import ProfileImg from '../../components/profileImg/ProfileImg';
+import UserInfoInput from '../../components/userInfoInput/UserInfoInput';
+import { ImgButton } from '../../components/imageButton/ImageButton';
 
-const ProfileModificationDiv = styled.div`
+const ProfileModificationWrap = styled.div`
     margin: 0 auto;
+    padding-top: 50px;
     width: 100%;
+    min-width: 358px;
 `;
 
-const TopBasicNav = styled.nav`
-    display: flex;
-    justify-content: space-between;
-    min-width: 390px;
-    border-bottom: 0.5px solid var(--border-color);
-`;
-
-const ArrowLeftImg = styled.img`
-    margin: 13px 0 10px 16px;
-    width: 22px;
-    height: 22px;
-`;
-
-const SaveBtn = styled.div`
-    margin: 8px 16px 8px 0;
-    padding: 7px 31px;
-    font-size: 14px;
-    font-weight: 500;
-    color: #ffffff;
-    border: 1px solid var(--border-color);
-    border-radius: 32px;
-    background-color: var(--main-color);
-`;
-
-const ProfileForm = styled.form`
-    margin: 0 auto;
-    padding: 30px 34px 0;
-`;
-
-const ImgDiv = styled.div`
+const ProfileHeader = styled.div`
     position: relative;
     margin: 0 auto;
-    padding-left: 10px;
+    padding-top: 30px;
     width: 275px;
     text-align: center;
 `;
 
-const ProfileImg = styled.img`
-    margin: 0 41px;
-    width: 110px;
-    height: 110px;
-`;
-
-const ChangeImg = styled.img`
-    position: absolute;
-    top: 74px;
-    left: 165px;
-    width: 36px;
-    height: 36px;
-`;
-
-const InputDiv = styled.div`
+const ProfileMain = styled.div`
+    display: flex;
+    flex-direction: column;
     margin: 0 auto;
-    padding-top: 14px;
-    width: fit-content;
-`;
-
-const UserInputLabel = styled.p`
-    margin: 16px 0 10px;
-    font-size: 12px;
-    font-weight: 500;
-    color: var(--sub-font);
-`;
-
-const UserInput = styled.input`
-    padding-bottom: 8px;
+    padding: 30px 0;
     width: 322px;
-    font-size: 14px;
-    font-weight: 400;
-    border: none;
-    border-bottom: 2px solid var(--border-color);
-
-    &:focus,
-    &:active {
-        outline: none;
-        border: none;
-        border-bottom: 2px solid var(--main-color);
-    }
-    &::placeholder {
-        font-size: 14px;
-        color: var(--border-color);
-    }
+    gap: 16px;
 `;
 
-const UserInputError = styled.strong`
-    display: block;
-    margin-top: 6px;
-    font-size: 12px;
-    color: var(--error-color);
+const UploadImageButton = styled(ImgButton)`
+    position: absolute;
+    top: 105px;
+    left: 155px;
+    padding: 0;
+    border: none;
 `;
 
 export default function ProfileModification() {
     return (
-        <ProfileModificationDiv>
-            <TopBasicNav>
-                <Link to='/myprofile' style={{ textDecoration: 'none' }}>
-                    <ArrowLeftImg src='/assets/icon/icon-arrow-left.png' alt='arrow-left-img' />
-                </Link>
-                <SaveBtn>저장</SaveBtn>
-            </TopBasicNav>
+        <>
+            <UploadHeader></UploadHeader>
 
-            <ProfileForm>
-                <ImgDiv>
-                    <ProfileImg src='/assets/Ellipse-1.png'></ProfileImg>
-                    <ChangeImg src='/assets/upload-file.png'></ChangeImg>
-                </ImgDiv>
+            <ProfileModificationWrap>
+                <ProfileHeader>
+                    <ProfileImg size='110px' src='../assets/Ellipse-1.png' alt='message'></ProfileImg>
+                    <UploadImageButton
+                        size='36px'
+                        src='../assets/upload-file.png'
+                        alt='upload button'
+                    ></UploadImageButton>
+                </ProfileHeader>
 
-                <InputDiv>
-                    <UserInputLabel htmlFor='name'>사용자 이름</UserInputLabel>
-                    <UserInput type='text' id='name' placeholder='2~10자 이내여야 합니다.'></UserInput>
-                    <UserInputError>*2~10자 이내여야 합니다.</UserInputError>
-                    <UserInputLabel htmlFor='id'>계정 ID</UserInputLabel>
-                    <UserInput
-                        type='text'
-                        id='id'
+                <ProfileMain>
+                    <UserInfoInput labelText='사용자 이름' placeholder='2~10자 이내여야 합니다.'></UserInfoInput>
+                    <UserInfoInput
+                        labelText='계정 ID'
                         placeholder='영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.'
-                    ></UserInput>
-                    <UserInputError>*영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.</UserInputError>
-                    <UserInputLabel htmlFor='desc'>소개</UserInputLabel>
-                    <UserInput type='text' id='desc' placeholder='자신에 대해 소개해 주세요!'></UserInput>
-                    <UserInputError>*최대 50자 입력 가능합니다.</UserInputError>
-                </InputDiv>
-            </ProfileForm>
-        </ProfileModificationDiv>
+                    ></UserInfoInput>
+                    <UserInfoInput
+                        labelText='소개'
+                        placeholder='자신과 판매할 상품에 대해 소개해 주세요!'
+                    ></UserInfoInput>
+                </ProfileMain>
+            </ProfileModificationWrap>
+        </>
     );
 }
