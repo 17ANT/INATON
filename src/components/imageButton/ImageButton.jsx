8ed555daf5c002559857a19b95747cac96ce0828
jangsumin <jsm0726@naver.com>
2022-12-15 16:19:59 +0900

    Refactor: 이미지 버튼 컴포넌트에서 공통 스타일만 남기고 삭제

--- src/components/imageButton/ImageButton.jsx
+++ src/components/imageButton/ImageButton.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const ImgButton = styled.img`
-    padding: 9px;
     width: ${(props) => props.size};
     height: ${(props) => props.size};
     border-radius: 50%;
-    border: 1px solid var(--border-color);
     cursor: pointer;
 `;
 
-export default function ImageButton({ size, src, alt }) {
-    return <ImgButton size={size} src={src} alt={alt}></ImgButton>;
+export default function ImageButton({ size, src, alt, onClick }) {
+    return <ImgButton size={size} src={src} alt={alt} onClick={onClick}></ImgButton>;
 }
