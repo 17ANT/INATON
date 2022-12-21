dd525836488d7490dfc0e05565d7dfe474c00eac
baek gyeonghyeon <baekg.dev@gmail.com>
2022-12-21 15:26:15 +0900

    Feat: 로그인 버튼 이동 처리

--- src/pages/LoginEmail/LoginEmail.jsx
+++ src/pages/LoginEmail/LoginEmail.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { BASE_URL } from '../../common/BASE_URL';
 import CustomButton from '../../components/customButton/CustomButton';
@@ -36,55 +37,43 @@ const SignupLink = styled.a`
 
 export default function LoginEmail() {
     const [user, setUser] = useState(null);
-    //    {user: {
-    //                     email: 'sewon123@naver.com',
-    //                     password: '12345678',
-    //                 },
+    const navigate = useNavigate();
     const emailRef = useRef(null);
     const pwRef = useRef(null);
+    const [msg, setMsg] = useState('');
 
-    // console.dir(emailRef);
-    // if (emailRef.current.value) {
-    //     emailRef.target.classList.add('active');
-    // }
     const handleChange = (e) => {
-        // console.log(emailRef.current.value);
-        // console.log(pwRef.current.value);
-
         if (emailRef.current.value !== '') {
             e.target.classList.add('active');
-        } else {
+        } else if (emailRef.current.value === '') {
             e.target.classList.remove('active');
         }
-
+        console.log(emailRef.current.value);
         setUser({ user: { email: emailRef.current.value, password: pwRef.current.value } });
     };
 
-    // useEffect(() => {
-    //     console.log(emailRef.current.value);
-    // }, []);
+    async function handleSubmit() {
+        // 데이터를 넘겨주면서 페이지 이동
+        const result = await postLogin(user);
+        if (result) {
+            navigate('/home');
+        } else {
+            setMsg('*이메일 또는 비밀번호가 일치하지 않습니다.');
+        }
+    }
 
-    //꼼수로 로드됐을 때 0.1초안에 화면 클릭이벤트 주기
-
-    // setTimeout(() => {
-    //     btn.console.log('클릭완');
-    // }, 1000);
-    // emailRef.click();
     return (
         <LoginForm>
             <h2>로그인</h2>
             <Form action="">
                 <InputDiv ref={emailRef} onChange={handleChange} text="이메일" type="email" />
                 <InputDiv ref={pwRef} onChange={handleChange} text="비밀번호" type="password" />
-                <ErrorMessage text={'*이메일 또는 비밀번호가 일치하지 않습니다.'} />
+                <ErrorMessage text={msg} />
                 <CustomButton
                     size="L"
-                    onClick={
-                        () => {
-                            postLogin(user);
-                        }
-                        // postLogin(user)
-                    }
+                    onClick={() => {
+                        handleSubmit();
+                    }}
                 >
                     로그인
                 </CustomButton>
