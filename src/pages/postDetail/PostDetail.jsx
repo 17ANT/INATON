195e95d43100ab3fcd8430f4afe5f397266c2f05
baek gyeonghyeon <baekg.dev@gmail.com>
2022-12-25 17:36:39 +0900

    Feat: postDetail 게시글 가져오기

--- src/pages/postDetail/PostDetail.jsx
+++ src/pages/postDetail/PostDetail.jsx
@@ -1,5 +1,7 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import getPost from '../../common/GetPostDetail';
 import Comment from '../../components/comment/Comment';
 import BasicHeader from '../../components/header/BasicHeader';
 import HomePost from '../../components/homePost/HomePost';
@@ -77,9 +79,21 @@ const CommentForm = styled.form`
 `;
 
 export default function PostDetail() {
+  const params = useParams();
+  const [postInfo, setPostInfo] = useState(null);
+  const [commentInfo, setCommentInfo] = useState(null);
+
+  async function getData(post_id) {
+    const postData = await getPost(params.post_id);
+    setPostInfo(postData.post);
+  }
+  getData();
+
   const [commentsValue, setCommentsValue] = useState('');
-  const inputRef = useRef(null)
-const[commentsList,setCommentsList] = useState([])
+  const inputRef = useRef(null);
+  const [commentsList, setCommentsList] = useState([]);
+  // 게시글 가져오기
+  // const postInfo = ;
 
   const handleCommentsValue = () => {
     // 댓글창 값 다루기
@@ -88,45 +102,33 @@ const[commentsList,setCommentsList] = useState([])
 
   const writeComments = (e) => {
     // 버튼 클릭 or 댓글 입력 후 엔터 이벤트시 댓글 작성
-    e.preventDefault(); 
+    e.preventDefault();
     CommentsWrite(commentsValue);
-    setCommentsValue('')
-    inputRef.current.value = ""
+    setCommentsValue('');
+    inputRef.current.value = '';
     console.log(commentsValue);
   };
 
   async function test() {
     // 댓글 리스트 불러오기
-      const comments = await CommentsList();
-      console.log(comments);
-      // setCommentsList(comments);
+    const comments = await CommentsList();
+    console.log(comments);
+    // setCommentsList(comments);
   }
-  test()
+  test();
+
   return (
     <>
       <BasicHeader />
       <PostDetailMain>
-        <HomePost
-          like="123456789"
-          comment="1234"
-          imgList={[
-            '/assets/chat-exapmle.png',
-            '/assets/post-img-example.png',
-            '/assets/post-img-example.png',
-          ]}
-        />
-
+        {postInfo && <HomePost data={postInfo} />}
         <CommentList>
-          <Comment
-            name="서귀포시 무슨 농장"
-            time="5"
-            src="./assets/post-img-example.png">
+          <Comment name="서귀포시 무슨 농장" time="5" src="./assets/post-img-example.png">
             게시글 답글 ~~ !! 최고최고
           </Comment>
           <Comment name="감귤러버" time="15" src="./assets/chat-exapmle.png">
-            안녕하세요. 사진이 너무 멋있어요. 한라봉 언제 먹을 수 있나요?
-            기다리기 지쳤어요 땡뻘땡뻘안녕하세요. 사진이 너무 멋있어요. 한라봉
-            언제 먹을 수 있나요? 기다리기 지쳤어요 땡뻘땡뻘
+            안녕하세요. 사진이 너무 멋있어요. 한라봉 언제 먹을 수 있나요? 기다리기 지쳤어요 땡뻘땡뻘안녕하세요. 사진이
+            너무 멋있어요. 한라봉 언제 먹을 수 있나요? 기다리기 지쳤어요 땡뻘땡뻘
           </Comment>
           {/* {commentsList &&
             commentsList.map((item) => (
@@ -142,14 +144,16 @@ const[commentsList,setCommentsList] = useState([])
           <CommentForm>
             <label htmlFor="inputComment" className="ir"></label>
             <input
-            ref={inputRef}
+              ref={inputRef}
               onChange={handleCommentsValue}
               type="text"
               placeholder="댓글 입력하기..."
               id="inputComment"
               required
             />
-            <button type='submit' onClick={writeComments}>게시</button>
+            <button type="submit" onClick={writeComments}>
+              게시
+            </button>
           </CommentForm>
         </CommentWrite>
       </PostDetailMain>
