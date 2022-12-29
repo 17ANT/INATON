6653b8b3feae4fca3da8e9ac240b11402c9723a6
baek gyeonghyeon <baekg.dev@gmail.com>
2022-12-29 11:18:54 +0900

    Feat: 8BM 이상 이미지 압축

---
+++ src/common/ImageResize.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import imageCompression from 'browser-image-compression';
+
+async function ImageResize(file) {
+  const options = {
+    maxSizeMB: 8,
+    maxWidhOrHeight: 1920,
+    useWebWorker: true,
+  };
+
+  try {
+    const blobFile = await imageCompression(file, options);
+    console.log(blobFile);
+    const newFile = new File([blobFile], `${blobFile.name}`, {
+      type: blobFile.type,
+    });
+    return newFile;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default ImageResize;
