1389e7b1abe98ebfa90858f03881555accdb36c2
baek gyeonghyeon <baekg.dev@gmail.com>
2023-01-02 14:36:11 +0900

    Feat: 에러 메시지 props 처리

--- src/components/errorMessage/ErrorMessage.jsx
+++ src/components/errorMessage/ErrorMessage.jsx
@@ -6,11 +6,7 @@ const WarningMessage = styled.span`
     props.colorname &&
     css`
       color: ${props.colorname};
-
-      /* color: var(--main-color); */
     `};
-  color: ${(props) => (props.colorname ? props.colorname : 'var(--error-color)')};
-  /* ${(props) => props.colorname && console.log('!', props.colorname)} */
   margin-top: 6px;
   font-weight: 500;
   font-size: 12px;
@@ -19,9 +15,4 @@ const WarningMessage = styled.span`
 
 export default function ErrorMessage({ text, color }) {
   return <WarningMessage colorname={color}>{text}</WarningMessage>;
-}
-
-// 이런식으로 사용하시면 됩니다
-{
-  /* <ErrorMessage text={"*이메일 또는 비밀번호가 일치하지 않습니다."} /> */
 }
